e1034206de87d723df5c2d810c5b9202a4075c38
Carlos Tapia <carlostapiaolguin1@gmail.com>
2018-08-08 16:32:37 -0400

    mensajes de error

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,24 +1,26 @@
-import {Component, OnInit, OnChanges, DoCheck} from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from "@angular/forms";
-import {LoggerService} from "../../service/utils/logger.service";
+import { Component, OnInit, OnChanges, DoCheck } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoggerService } from '../../service/utils/logger.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
-export class CreateComponent implements OnInit, DoCheck{
+export class CreateComponent implements OnInit {
 
   public form: FormGroup;
 
-  ngDoCheck(): void {
-    this.form.get('name').hasError('required');
-  }
+  validation_messages = {
+    'name': [
+      { type: 'required', message: 'Nombre es requerido' },
+      { type: 'minlength', message: 'nombre no puede tener menos de 8 caracteres' }
+    ]
+  };
 
-
-  constructor(_fb: FormBuilder, _log : LoggerService) {
+  constructor(_fb: FormBuilder, _log: LoggerService) {
     this.form = _fb.group({
-      name : ['', [Validators.required, Validators.minLength(8)]]
+      name: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
 
@@ -26,8 +28,8 @@ export class CreateComponent implements OnInit, DoCheck{
   }
 
 
-  public create(){
-    _log.debug("asasasas");
+  public create() {
+    this._log.debug('asasasas');
   }
 
 }
--- src/app/service/utils/logger.service.ts
+++ src/app/service/utils/logger.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import {environment} from "../../../environments/environment";
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class LoggerService {
 
   constructor() { }
 
-  public debug(mensage : string, ...optionalParams: any[]){
-    if (environment.production) return;
+  public debug(mensage: string, ...optionalParams: any[]) {
+    if (environment.production) { return; }
     console.debug(mensage, optionalParams);
   }
 
